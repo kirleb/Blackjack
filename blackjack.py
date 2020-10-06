b35d85b44aca38d1bbcea898ebdc52f2cb28cffa
Aarron Stephenson <caleb.stephenson@live.co.uk>
2020-10-06 11:25:57 +0100

    updated blackjack.py and hand.py

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,10 @@
 from card import Card
 from random import shuffle
+from hand import Hand
 '''
 This plays a blackjack game
 '''
-def generate_deck():
+def generate_deck(): #maybe make this a static method in Deck
     '''returns an unshuffled list of Card objects'''
     suits = ('Hearts', 'Spades', 'Clubs', 'Diamonds') 
     ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
@@ -12,13 +13,46 @@ def generate_deck():
     play_deck = []
     for suit in suits:
         for rank in ranks:
-            if rank == 'Ace':
-                new_card = Card(suit,rank,True)
-                play_deck.append(new_card)
-                continue
-            new_card = Card(suit,rank,False)
+            is_ace = True if rank == 'Ace' else False
+            new_card = Card(suit,rank,is_ace)
             play_deck.append(new_card)
     return play_deck
+
+def retrieve_number_of_players():
+    '''Asks how many players and returns number of players as an integer'''
+    
+    while True:
+        print('Please Select a integer between 1-4')
+        try:
+            number_of_players = int(input('Number of Players?: '))
+            if number_of_players > 0 and number_of_players < 5:
+                return number_of_players
+            print('That\'s not between 1-4')
+        except ValueError:
+            print('That was not a integer')
+
+def retrieve_player_names(number_of_players):
+    '''
+    Takes number of players, asks for players names and returns them in a list
+    any name is fine as long as it isn't a duplicate, dealer or empty
+    '''
+    player_names = []
+    while len(player_names) < number_of_players:
+        new_name = input(f'Enter a Name{len(player_names)+1}: ')
+        if new_name == 'dealer':
+            print('You cannot be the dealer')
+        elif (new_name not in player_names):
+            player_names.append(new_name)
+        else:
+            print('Ensure your name is unique')
+    return player_names
+
+def generate_hands(player_names,play_deck):
+    names_hands = {name: Hand(name,play_deck) for name in player_names}
+    return names_hands
+
+
+
 
 
 def main():
@@ -26,6 +60,8 @@ def main():
 
 if __name__ == '__main__':
     main()
+    player_names = retrieve_player_names(retrieve_number_of_players())
     play_deck = generate_deck()
     shuffle(play_deck)
-    print(play_deck.pop())
+    for name,hand in generate_hands(player_names,play_deck).items():
+        print(hand)
--- hand.py
+++ hand.py
@@ -4,7 +4,7 @@ class Hand:
     Upon construction 2 cards are immediately added.
     """
 
-    def __init__(self,name):
+    def __init__(self,name,deck):
 
         self.is_dealer = False
         if name == 'dealer':
@@ -12,26 +12,36 @@ class Hand:
         self.cards = []
         self.score = 0
         self.aces = 0
+        self.name = name
 
-        self.hit()
-        self.hit()
+        self.hit(deck)
+        self.hit(deck)
 
     def __str__(self):
         '''Returns a different string depending on if it is a player or dealer hand'''
-        if is_dealer:
-            return f'The dealer\'s cards are {" ".join(self.cards)}\nTheir score is {self.score}'
-        return f'{self.name}\'s cards are {" ".join(self.cards)}\n{self.name}\'s score is {self.score}'
+        card_list = [str(card) for card in self.cards]   
+        if self.is_dealer:
+            return f'The dealer\'s cards are \n'+',\n'.join(card_list)+f'\nTheir score is {self.score}'
+        return f'{self.name}\'s cards are \n'+',\n'.join(card_list)+f'\n{self.name}\'s score is {self.score}'
 
-    def hit(self,card):
-        '''Hand recieves a card from the deck, updates the aces count and the hands score '''
+    def hit(self,deck):
+        '''Takes a deck, Hand recieves a card from the deck, updates the aces count and the hands score '''
+        rank_values_pairs = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7,
+        'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+
+        card = deck.pop()
         self.cards.append(card)
 
         if card.is_ace:
             self.aces += 1
 
-        self.score += rank_value_pairs[card.rank]
+        self.score += rank_values_pairs[card.rank]
 
-    rank_values_pairs = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7,
-    'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+        if self.score > 21 and self.aces > 1:
+            self.score -= 10
+            self.aces -= 1
+
+
+    
         
 
